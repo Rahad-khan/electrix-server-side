e833b97c766dfa455a4aa7f0027e0cf634578b5f
Rahad Khan <clash146098@gmail.com>
2022-05-23 03:48:26 +0600

    individual product loaded by id

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 
 
@@ -12,6 +13,38 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@toolkits.jrid37h.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db("toolkit").collection("products");
+
+
+        app.get("/products", async (req, res) => {
+            const result = await productCollection.find({}).toArray();
+            res.send(result);
+        });
+
+        app.get("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        });
+    }
+    finally {
+        // console.log("object");
+    }
+}
+
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send("Manufacturer Tool is Ready")
 });
